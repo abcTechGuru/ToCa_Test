e0fd16d05c0fdc576ad2f7d44a94272a7c19808d
toTechGuru <roman.borsuk.guru@gmail.com>
2025-05-30 00:06:23 +0300

    'lazy-loading'

--- src/components/utility/SanityImageBlock.tsx
+++ src/components/utility/SanityImageBlock.tsx
@@ -1,3 +1,6 @@
+'use client';
+
+import { useState } from 'react';
 import { cn } from '@/libs/functions';
 import decodeSanityImage from '@/sanity/lib/decodeSanityImage';
 import type { SanityImage } from '@/sanity/types';
@@ -5,21 +8,35 @@ import Image from 'next/image';
 
 export default function SanityImageBlock({
   image,
+  className,
   ...props
 }: {
   image: SanityImage;
 } & Omit<React.ComponentProps<typeof Image>, 'src' | 'alt'>) {
+  const [isLoaded, setIsLoaded] = useState(false);
   const sourceImage = decodeSanityImage(image);
-  const { fill, width, height, className } = props;
+  const { fill, width, height } = props;
 
   return (
-    <Image
-      src={sourceImage.url}
-      className={cn(className)}
-      alt={image.alt || ''}
-      width={!fill ? width || sourceImage.dimensions.width : undefined}
-      height={!fill ? height || sourceImage.dimensions.height : undefined}
-      {...props}
-    />
+    <div className={cn(className, 'relative', fill && 'w-full h-full')}>
+      {!isLoaded && (
+        <div className="absolute inset-0 animate-pulse bg-[var(--dark-gray)]" />
+      )}
+      <Image
+        src={sourceImage.url}
+        alt={image.alt || ''}
+        onLoad={() => setIsLoaded(true)}
+        className={cn(
+          'absolute inset-0 w-full h-full object-cover transition-opacity duration-500 ease-in-out',
+          className,
+          isLoaded ? 'opacity-100' : 'opacity-0'
+        )}
+        width={!fill ? width || sourceImage.dimensions.width : undefined}
+        height={!fill ? height || sourceImage.dimensions.height : undefined}
+        fill={fill}
+        {...props}
+      />
+
+    </div>
   );
 }
